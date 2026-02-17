d6dd71f668c8204d7c517fb572110e54915c8929
janitha1996 <119646255+janitha-mahanthe@users.noreply.github.com>
2026-02-17 13:55:03 -0500

    Use relative cache import and update ASCII art
    Replace the absolute import with a relative import (from .cache) so the cache deletion works correctly when the module is executed as a package. Also replace the ASCII_Mupt_reaction_LAMMPS banner with an updated, reflowed ASCII art block (visual/layout changes only).

--- src/AutoREACTER/initialization.py
+++ src/AutoREACTER/initialization.py
@@ -1,30 +1,30 @@
 import importlib
 import sys
 try:  # for running as a package
-    from autoREACT.cache import delete_default_cache_files as delete_cache_dir
+    from .cache import delete_default_cache_files as delete_cache_dir
 except ImportError:  # for running as a script
     from cache import delete_default_cache_files as delete_cache_dir
 
 
 def ASCII_Mupt_reaction_LAMMPS():
-    ascii_art = r"""                                                                                                                                                                                                             
-                                                                                                                                                                                                             
-               AAA                                     tttt                                RRRRRRRRRRRRRRRRR   EEEEEEEEEEEEEEEEEEEEEE               AAA                  CCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTT
-              A:::A                                 ttt:::t                                R::::::::::::::::R  E::::::::::::::::::::E              A:::A              CCC::::::::::::CT:::::::::::::::::::::T
-             A:::::A                                t:::::t                                R::::::RRRRRR:::::R E::::::::::::::::::::E             A:::::A           CC:::::::::::::::CT:::::::::::::::::::::T
-            A:::::::A                               t:::::t                                RR:::::R     R:::::REE::::::EEEEEEEEE::::E            A:::::::A         C:::::CCCCCCCC::::CT:::::TT:::::::TT:::::T
-           A:::::::::A        uuuuuu    uuuuuuttttttt:::::ttttttt       ooooooooooo          R::::R     R:::::R  E:::::E       EEEEEE           A:::::::::A       C:::::C       CCCCCCTTTTTT  T:::::T  TTTTTT
-          A:::::A:::::A       u::::u    u::::ut:::::::::::::::::t     oo:::::::::::oo        R::::R     R:::::R  E:::::E                       A:::::A:::::A     C:::::C                      T:::::T        
-         A:::::A A:::::A      u::::u    u::::ut:::::::::::::::::t    o:::::::::::::::o       R::::RRRRRR:::::R   E::::::EEEEEEEEEE            A:::::A A:::::A    C:::::C                      T:::::T        
-        A:::::A   A:::::A     u::::u    u::::utttttt:::::::tttttt    o:::::ooooo:::::o       R:::::::::::::RR    E:::::::::::::::E           A:::::A   A:::::A   C:::::C                      T:::::T        
-       A:::::A     A:::::A    u::::u    u::::u      t:::::t          o::::o     o::::o       R::::RRRRRR:::::R   E:::::::::::::::E          A:::::A     A:::::A  C:::::C                      T:::::T        
-      A:::::AAAAAAAAA:::::A   u::::u    u::::u      t:::::t          o::::o     o::::o       R::::R     R:::::R  E::::::EEEEEEEEEE         A:::::AAAAAAAAA:::::A C:::::C                      T:::::T        
-     A:::::::::::::::::::::A  u::::u    u::::u      t:::::t          o::::o     o::::o       R::::R     R:::::R  E:::::E                  A:::::::::::::::::::::AC:::::C                      T:::::T        
-    A:::::AAAAAAAAAAAAA:::::A u:::::uuuu:::::u      t:::::t    tttttto::::o     o::::o       R::::R     R:::::R  E:::::E       EEEEEE    A:::::AAAAAAAAAAAAA:::::AC:::::C       CCCCCC        T:::::T        
-   A:::::A             A:::::Au:::::::::::::::uu    t::::::tttt:::::to:::::ooooo:::::o     RR:::::R     R:::::REE::::::EEEEEEEE:::::E   A:::::A             A:::::AC:::::CCCCCCCC::::C      TT:::::::TT      
-  A:::::A               A:::::Au:::::::::::::::u    tt::::::::::::::to:::::::::::::::o     R::::::R     R:::::RE::::::::::::::::::::E  A:::::A               A:::::ACC:::::::::::::::C      T:::::::::T      
- A:::::A                 A:::::Auu::::::::uu:::u      tt:::::::::::tt oo:::::::::::oo      R::::::R     R:::::RE::::::::::::::::::::E A:::::A                 A:::::A CCC::::::::::::C      T:::::::::T      
-AAAAAAA                   AAAAAAA uuuuuuuu  uuuu        ttttttttttt     ooooooooooo        RRRRRRRR     RRRRRRREEEEEEEEEEEEEEEEEEEEEEAAAAAAA                   AAAAAAA   CCCCCCCCCCCCC      TTTTTTTTTTT      
+    ascii_art = r"""                                                                                                                                                                                                                                                       
+                                                                                                                                                                                                                                                       
+               AAA                                     tttt                                RRRRRRRRRRRRRRRRR   EEEEEEEEEEEEEEEEEEEEEE               AAA                  CCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
+              A:::A                                 ttt:::t                                R::::::::::::::::R  E::::::::::::::::::::E              A:::A              CCC::::::::::::CT:::::::::::::::::::::TE::::::::::::::::::::ER::::::::::::::::R  
+             A:::::A                                t:::::t                                R::::::RRRRRR:::::R E::::::::::::::::::::E             A:::::A           CC:::::::::::::::CT:::::::::::::::::::::TE::::::::::::::::::::ER::::::RRRRRR:::::R 
+            A:::::::A                               t:::::t                                RR:::::R     R:::::REE::::::EEEEEEEEE::::E            A:::::::A         C:::::CCCCCCCC::::CT:::::TT:::::::TT:::::TEE::::::EEEEEEEEE::::ERR:::::R     R:::::R
+           A:::::::::A        uuuuuu    uuuuuuttttttt:::::ttttttt       ooooooooooo          R::::R     R:::::R  E:::::E       EEEEEE           A:::::::::A       C:::::C       CCCCCCTTTTTT  T:::::T  TTTTTT  E:::::E       EEEEEE  R::::R     R:::::R
+          A:::::A:::::A       u::::u    u::::ut:::::::::::::::::t     oo:::::::::::oo        R::::R     R:::::R  E:::::E                       A:::::A:::::A     C:::::C                      T:::::T          E:::::E               R::::R     R:::::R
+         A:::::A A:::::A      u::::u    u::::ut:::::::::::::::::t    o:::::::::::::::o       R::::RRRRRR:::::R   E::::::EEEEEEEEEE            A:::::A A:::::A    C:::::C                      T:::::T          E::::::EEEEEEEEEE     R::::RRRRRR:::::R 
+        A:::::A   A:::::A     u::::u    u::::utttttt:::::::tttttt    o:::::ooooo:::::o       R:::::::::::::RR    E:::::::::::::::E           A:::::A   A:::::A   C:::::C                      T:::::T          E:::::::::::::::E     R:::::::::::::RR  
+       A:::::A     A:::::A    u::::u    u::::u      t:::::t          o::::o     o::::o       R::::RRRRRR:::::R   E:::::::::::::::E          A:::::A     A:::::A  C:::::C                      T:::::T          E:::::::::::::::E     R::::RRRRRR:::::R 
+      A:::::AAAAAAAAA:::::A   u::::u    u::::u      t:::::t          o::::o     o::::o       R::::R     R:::::R  E::::::EEEEEEEEEE         A:::::AAAAAAAAA:::::A C:::::C                      T:::::T          E::::::EEEEEEEEEE     R::::R     R:::::R
+     A:::::::::::::::::::::A  u::::u    u::::u      t:::::t          o::::o     o::::o       R::::R     R:::::R  E:::::E                  A:::::::::::::::::::::AC:::::C                      T:::::T          E:::::E               R::::R     R:::::R
+    A:::::AAAAAAAAAAAAA:::::A u:::::uuuu:::::u      t:::::t    tttttto::::o     o::::o       R::::R     R:::::R  E:::::E       EEEEEE    A:::::AAAAAAAAAAAAA:::::AC:::::C       CCCCCC        T:::::T          E:::::E       EEEEEE  R::::R     R:::::R
+   A:::::A             A:::::Au:::::::::::::::uu    t::::::tttt:::::to:::::ooooo:::::o     RR:::::R     R:::::REE::::::EEEEEEEE:::::E   A:::::A             A:::::AC:::::CCCCCCCC::::C      TT:::::::TT      EE::::::EEEEEEEE:::::ERR:::::R     R:::::R
+  A:::::A               A:::::Au:::::::::::::::u    tt::::::::::::::to:::::::::::::::o     R::::::R     R:::::RE::::::::::::::::::::E  A:::::A               A:::::ACC:::::::::::::::C      T:::::::::T      E::::::::::::::::::::ER::::::R     R:::::R
+ A:::::A                 A:::::Auu::::::::uu:::u      tt:::::::::::tt oo:::::::::::oo      R::::::R     R:::::RE::::::::::::::::::::E A:::::A                 A:::::A CCC::::::::::::C      T:::::::::T      E::::::::::::::::::::ER::::::R     R:::::R
+AAAAAAA                   AAAAAAA uuuuuuuu  uuuu        ttttttttttt     ooooooooooo        RRRRRRRR     RRRRRRREEEEEEEEEEEEEEEEEEEEEEAAAAAAA                   AAAAAAA   CCCCCCCCCCCCC      TTTTTTTTTTT      EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
 """
     print(ascii_art)
 
