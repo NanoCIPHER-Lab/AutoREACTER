b1d2bfd8c6b81d55a3acdcb9859c889828437936
janitha1996 <119646255+janitha-mahanthe@users.noreply.github.com>
2026-04-12 21:30:46 -0400

    Update lammps_settings.py

--- AutoREACTER/sim_setup/writers/lammps_settings.py
+++ AutoREACTER/sim_setup/writers/lammps_settings.py
@@ -1,25 +1,50 @@
 from dataclasses import dataclass
 from pathlib import Paths
+
+from cairo import Path
 from AutoREACTER.reaction_preparation.lunar_client.REACTER_files_builder import REACTERFiles
 
 @dataclass(slots=True)
 class LammpsSettings:
-    units: str = "real"
-    dimension: str = "3"
-    boundary: str = "p p p"
-    atom_style: str = "full"
-    newton: str = "on"
-    bond_style: str = "class2"
-    angle_style: str = "class2"
-    dihedral_style: str = "class2"
-    improper_style: str = "class2"
-    special_bonds: str = "lj/coul 0 0 1"
-    pair_style: str = "lj/class2/coul/long 8.5"
-    kspace_style: str = "pppm 1.0e-4"
-    pair_modify: str = "tail yes mix sixthpower"
-    neighbor: str = "2.0 bin"
-    neigh_modify: str = "delay 0 every 1 check yes one 5000 page 100000"
+    units: str 
+    dimension: str 
+    boundary: str 
+    atom_style: str 
+    bond_style: str 
+    angle_style: str
+    dihedral_style: str
+    improper_style: str
+    special_bonds: str
+    pair_style: str 
+    kspace_style: str
+    pair_modify: str 
+    neighbor: str
+    neigh_modify: str 
 
 class InitialSetupWriter:
     def __init__(self, reacter_files: REACTERFiles):
         self.reacter_files = reacter_files
+        lunar_in_file_location = reacter_files.in_file
+
+    def _defults(self) -> LammpsSettings:
+        return LammpsSettings(
+            units="real",
+            dimension="3",
+            boundary="p p p",
+            atom_style="full",
+            bond_style="harmonic",
+            angle_style="harmonic",
+            dihedral_style="opls",
+            improper_style="cvff",
+            special_bonds="lj/coul 0.0 0.0 0.5",
+            pair_style="lj/cut/coul/long 10.0 10.0",
+            kspace_style="pppm 1e-4",
+            pair_modify="mix arithmetic",
+            neighbor="2.0 bin",
+            neigh_modify="delay 5 every 1"
+        )
+
+    def _get_LUNAR_lammps_settings(self, output_path: Path) -> None:
+        with open(output_path, 'w') as f:
+            pass
+
